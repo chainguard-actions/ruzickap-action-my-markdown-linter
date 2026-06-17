ef9ca3e6c1719e753a985984ae04918dc43699ab
actionchain <actionchain@chainguard.dev>
2026-06-17 01:04:13 +0000

    Update ruzickap/action-my-markdown-linter@v1.0.9

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM node:current-alpine
+
+LABEL maintainer="Petr Ruzicka <petr.ruzicka@gmail.com>"
+LABEL repository="https://github.com/ruzickap/action-my-markdown-linter"
+LABEL homepage="https://github.com/ruzickap/action-my-markdown-linter"
+
+LABEL "com.github.actions.name"="My Markdown Linter"
+LABEL "com.github.actions.description"="Lint Markdown files"
+LABEL "com.github.actions.icon"="list"
+LABEL "com.github.actions.color"="blue"
+
+# renovate: datasource=npm depName=markdownlint-cli versioning=npm
+ENV MARKDOWNLINT_CLI_VERSION="0.36.0"
+
+SHELL ["/bin/ash", "-eo", "pipefail", "-c"]
+
+RUN set -eux && \
+    apk --update --no-cache add bash fd && \
+    npm install --global --production "markdownlint-cli@v${MARKDOWNLINT_CLI_VERSION}"
+
+COPY entrypoint.sh /entrypoint.sh
+
+WORKDIR /mnt
+ENTRYPOINT [ "/entrypoint.sh" ]
---
+++ entrypoint.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+# Config file for markdownlint-cli (it's using .markdownlint.json/.markdownlint.yaml/.markdownlint.yml by default)
+export CONFIG_FILE=${INPUT_CONFIG_FILE:-}
+
+# Debug variable - enable by setting non-empty value
+export DEBUG=${INPUT_DEBUG:-}
+
+# Exclude files or directories which should not be linted
+export EXCLUDE=${INPUT_EXCLUDE:-}
+
+# Command line parameters for fd ("exclude" and "search_paths" parameters are ignored if this is set)
+export FD_CMD_PARAMS="${INPUT_FD_CMD_PARAMS:- . -0 --extension md --type f --hidden --no-ignore --exclude .git/}"
+
+# Files or paths which will be linted
+export SEARCH_PATHS=${INPUT_SEARCH_PATHS:-}
+
+print_error() {
+  echo -e "\e[31m*** ERROR: ${1}\e[m"
+}
+
+print_info() {
+  echo -e "\e[36m*** ${1}\e[m"
+}
+
+error_trap() {
+  print_error "Something went wrong - see the errors above..."
+  exit 1
+}
+
+################
+# Main
+################
+
+trap error_trap ERR
+
+[ -n "${DEBUG}" ] && set -x
+
+IFS=' ' read -r -a FD_CMD_PARAMS <<< "$FD_CMD_PARAMS"
+
+# Change FD_CMD_PARAMS variable only if INPUT_FD_CMD_PARAMS was not defined or is empty
+if [ -z "${INPUT_FD_CMD_PARAMS+x}" ] || [ -z "${INPUT_FD_CMD_PARAMS}" ]; then
+  if [ -n "${EXCLUDE}" ]; then
+    for EXCLUDED in ${EXCLUDE}; do
+      FD_CMD_PARAMS+=("--exclude" "${EXCLUDED}")
+    done
+  fi
+
+  if [ -n "${SEARCH_PATHS}" ]; then
+    for SEARCH_PATH in ${SEARCH_PATHS}; do
+      FD_CMD_PARAMS+=("${SEARCH_PATH}")
+    done
+  fi
+fi
+
+declare -a MARKDOWNLINT_CMD_PARAMS
+if [ -n "${CONFIG_FILE}" ]; then
+  MARKDOWNLINT_CMD_PARAMS+=("--config" "${CONFIG_FILE}")
+fi
+
+print_info "Start checking..."
+print_info "Running: fd ${FD_CMD_PARAMS[*]}"
+
+while IFS= read -r -d '' FILE; do
+  print_info "Running: markdownlint ${MARKDOWNLINT_CMD_PARAMS[*]} ${FILE}"
+  markdownlint "${MARKDOWNLINT_CMD_PARAMS[@]}" "${FILE}"
+done < <(fd "${FD_CMD_PARAMS[@]}")
+
+print_info "Checks completed..."
